1abae568feba5d30d2c94261545bb0df588d37a7
Shadow <dark35854@gmail.com>
2024-12-27 22:26:57 +0530

    feat(added_animation_)

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -6,7 +6,7 @@ interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
 
 export const Heading = ({children,className,...props}:HeadingProps) => {
   return (
-    <h1 className={cn("text-4xl sm:text-5xl text-pretty font-semibold tracking-tight text-zinc-800",className)}
+    <h1 className={cn("text-4xl sm:text-5xl text-pretty font-semibold tracking-tight text-zinc-800 dark:text-foreground",className)}
     {...props}>
         {children}
     </h1>
