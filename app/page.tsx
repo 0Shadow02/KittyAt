54f09c0226975c2a4f241935dd405a1e7c128a0b
Shadow <dark35854@gmail.com>
2024-12-27 22:25:53 +0530

    feat(landing_page)

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,68 @@
-import Image from "next/image";
+import { ModeToggle } from "@/components/ThemeSwitcherBtn"
+import {Heading} from "@/components/Heading"
+import { TextEffect } from "@/components/ui/text-effect"
+import { TextShimmer } from "@/components/motion-ui/TextShimmerColor"
+import { CheckSquare2Icon } from "lucide-react"
+import ShinyButton from "@/components/motion/ShinyButton"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+function page() {
+  return  (
+    <div className="relative h-screen w-full flex flex-col">
+      <ModeToggle />
+      <section className="flex flex-col items-center justify-center py-24 sm:py-32 text-center w-full px-3.5 md:px-20" >
+        <div className="my-10">
+          <Heading>
+            <span>
+              <TextEffect per='char' preset='fade-in-blur'>
+                Real-Time Insights for Cloud-Based Services
+              </TextEffect>
+            </span>
+              
+            <span>
+              <TextShimmer
+                duration={1.2}
+                className="py-4 [--base-color:theme(colors.blue.600)] [--base-gradient-color:theme(colors.blue.200)] dark:[--base-color:theme(colors.blue.700)] dark:[--base-gradient-color:theme(colors.blue.400)]"
+              >
+                Sent to Your Discord
+              </TextShimmer>
+            </span>
+           
+          </Heading>
+         </div>
+            <p className=" flex  flex-col justify-center items-center text-lg text-gray-600 dark:text-gray-300 max-w-prose text-center text-pretty">
+              KittyAt is the easiest way to monitor your SaaS , Get instant notification for{" "}
+              {" "}
+            <span className=" font-semibold text-foreground text-lg">
+              sales, new users, or any other event
+            </span> 
+              </p>
+          <ul className=" font-semibold space-y-2 text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 text-left flex flex-col items-start">
+            {[
+              "Real-time Discord alerts for critical events",
+              "Buy once , use forever",
+              "Track sales, new users, or any other event"
+            ].map((item,index)=> (
+              <li key={index} className=" flex gap-1.5 items-center text-left">
+                <CheckSquare2Icon className=" size-5 shrink-0 text-blue-500"/>
+                {item}
+              </li>
+            ))}
+          </ul>
+          <div>
+          <ShinyButton>
+           Start free from today
+          </ShinyButton>
+
+    
+          </div>
+           
+      </section>
     </div>
   );
-}
+};
+
+export default page
+
+
