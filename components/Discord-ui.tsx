c7be179d184f6116bf71820144e9e107777dfd71
Shadow <dark35854@gmail.com>
2025-01-02 22:34:49 +0530

    (animated-list_and_discord-message)

--- components/Discord-ui.tsx
+++ components/Discord-ui.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Icons from './icons';
-import { Inbox, PlusCircle, UserCircle2 } from 'lucide-react';
+import { Cog, Gift, Headphones, HelpCircle, Inbox, Menu, Mic, Phone, Pin, PlusCircle, Search, Smile, Sticker, UserCircle2, Video } from 'lucide-react';
 import Image from 'next/image';
 
 const DiscordUi = ({ children }: { children: React.ReactNode }) => {
@@ -32,41 +32,109 @@ const DiscordUi = ({ children }: { children: React.ReactNode }) => {
             </div>
           </div>
 
-          <div className="hidden md:flex w-60 bg-[#2f3137] flex-col">
-            <div className="px-4 h-16 border-b border-[#202225] flex items-center shadow-sm">
-              <div className="w-full bg-[#202225] text-sm rounded px-2 h-8 flex items-center justify-center text-gray-500 cursor-not-allowed">
-                Find or start a conversation
+          <div className="hidden md:flex w-60 bg-[#2f3137] flex-col justify-between">
+            <div>
+              <div className="px-4 h-16 border-b border-[#202225] flex items-center shadow-sm">
+                <div className="w-full bg-[#202225] text-sm rounded px-2 h-8 flex items-center justify-center text-gray-500 cursor-not-allowed">
+                  Find or start a conversation
+                </div>
+              </div>
+
+              <div className="flex-1 overflow-y-auto pt-4">
+                <div className="px-2 mb-4">
+                  <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcddee] cursor-not-allowed">
+                    <UserCircle2 className="mr-4 size-8 text-[#b9bbbe]" />
+                    <span className="font-medium text-sm">Friends</span>
+                  </div>
+                  <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcddee] cursor-not-allowed">
+                    <Inbox className="mr-4 size-8 text-[#b9bbbe]" />
+                    <span className="font-medium text-sm">Nitro</span>
+                  </div>
+                </div>
+
+                <div className='px-2 mb-4'>
+                  <h3 className='text-xs font-semibold text-[#8e9297] px-2 mb-2 uppercase'>
+                    Direct Messages
+                  </h3>
+                  <div className='flex items-center px-2 py-1.5 rounded bg-[#393c43] text-white cursor-pointer'>
+                    <Image src="/asset-profile-picture.jpeg" alt="KittyAt_Avt"
+                      width={32}
+                      height={32}
+                      className='object-cover rounded-full mr-3'
+                    />
+                    <span className='font-medium'>KittyAt</span>
+                  </div>
+                  <div className='my-1 space-y-px'>
+                    {[...Array(4)].map((_, i) => (
+                      <div key={i}
+                        className='flex items-center px-2 py-1.5 rounded text-gray-600 cursor-not-allowed'>
+                        <div className='size-8 rounded-full bg-discord-background mr-3' />
+                        <span className='font-medium'>User {i + 1}</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
-
-            <div className="flex-1 overflow-y-auto pt-4">
-              <div className="px-2 mb-4">
-                <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcddee] cursor-not-allowed">
-                  <UserCircle2 className="mr-4 size-8 text-[#b9bbbe]" />
-                  <span className="font-medium text-sm">Friends</span>
-                </div>
-                <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcddee] cursor-not-allowed">
-                  <Inbox className="mr-4 size-8 text-[#b9bbbe]" />
-                  <span className="font-medium text-sm">Nitro</span>
-                </div>
+            <div className='p-2 bg-[#292b2f] flex items-center'>
+              <div className='size-8 rounded-full bg-[#3659B1] mr-2' />
+              <div className='flex-1'>
+                <p className='text-sm font-medium text-white'>You</p>
+                <p className='text-xs text-[#b9bbbe] flex items-center'>@your_account</p>
               </div>
-
-              <div className=' px-2 mb-4 '>
-                 <h3 className=' text-xs font-semibold text-[#8e9297] px-2 mb-2 uppercase '>
-                    Direct Messages
-                 </h3>
-                 <div className=' flex items-center px-2 py-1.5 rounded bg-[#393c43] text-white cursor-pointer' >
-                    <Image src="/asset-profile-picture.png" alt="KittyAt_Avt"
-                     width={32}
-                     height={32}
-                     className=' object-cover rounded-full mr-3'
-                    />
-
-                 </div>
-
+              <div className=' flex items-center space-x-2'>
+                <Mic className=' size-5 text-[#b9bbbe] hover:text-white cursor-pointer' />
+                <Headphones className=' size-5 text-[#b9bbbe] hover:text-white cursor-pointer' />
+                <Cog className=' size-5 text-[#b9bbbe] hover:text-white cursor-pointer' />
               </div>
             </div>
           </div>
+           <div className=' flex-1 flex flex-col'>
+            <div className=' h-16 bg-[#36393f] flex items-center px-4 shadow-sm border-b border-[#202225]'>
+                <div className=' md:hidden mr-4'>
+                    <Menu className=' size-6 text-[#b9bbbe] hover:text-white cursor-pointer'/>
+                </div>                
+                 <div className=' flex items-center'>
+                  <div className='relative'>
+                   <Image src="/asset-profile-picture.jpeg" alt="KittyAt_Avt"
+                      width={40}
+                      height={40}
+                      className='object-cover rounded-full mr-3'
+                    />
+                    <div className=' absolute bottom-0 right-3 size-3 bg-green-500 rounded-full border-2 border-[#36393f]' />
+                  </div>
+
+                   <p className=' font-semibold text-white'>KittyAt</p>
+                 </div>
+
+                 <div className=' ml-auto flex items-center space-x-4 text-[#b9bbbe]'>
+                    <Phone className=' size-5 hover:text-white cursor-not-allowed hidden sm:block' />
+                    <Video className=' size-5 hover:text-white cursor-not-allowed hidden sm:block' />
+                    <Pin className=' size-5 hover:text-white cursor-not-allowed hidden sm:block' />
+                    <UserCircle2 className=' size-5 hover:text-white cursor-not-allowed hidden sm:block' />
+                    <Search className=' size-5 hover:text-white cursor-not-allowed hidden sm:block' />
+                    <Inbox className=' size-5 hover:text-white cursor-not-allowed hidden sm:block' />
+                    <HelpCircle className=' size-5 hover:text-white cursor-not-allowed hidden sm:block' />
+                 </div>
+            </div>
+              {/* message history */}
+              <div className=' flex-1 overflow-y-auto p-4 bg-discord-background flex flex-col-reverse'>
+                    {children}
+              </div>
+              {/* Message Input */}
+              <div className=' p-4'>
+                <div className=' flex items-center bg-[#40444b] rounded-lg p-1'>
+                    <PlusCircle className=' mx-3 text-[#b9bbbe] hover:text-white cursor-not-allowed' />
+                    <input readOnly type='text' placeholder='Message @KittyAt'
+                    className=' flex-1 bg-transparent py-2.5 px-1 text-white placeholder-[#72767d] focus:outline-none cursor-not-allowed' />
+                    <div className=' flex items-center space-x-3 mx-3 text-[#b9bbbe]'>
+                      <Gift className='sizw-5 hover:text-white cursor-not-allowed hidden sm:block' />
+                      <Sticker className='sizw-5 hover:text-white cursor-not-allowed hidden sm:block' />
+                      <Smile className='sizw-5 hover:text-white cursor-not-allowed hidden sm:block' />
+                    </div>
+                </div>
+              </div>
+           </div>
         </div>
       </div>
     </div>
