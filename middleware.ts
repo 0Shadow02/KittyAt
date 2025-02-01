1e61c217dd66601098a99d6aabbfb76926f1026b
Shadow <dark35854@gmail.com>
2025-02-01 18:31:57 +0530

    feat:fixed_error

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,46 @@
 import NextAuth from "next-auth";
+
 import authConfig from "@/auth.config";
-import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from "@/routes";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  const { nextUrl, auth: isLoggedIn } = req;
-  const path = nextUrl.pathname;
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-  if (path.startsWith(apiAuthPrefix)) return;
-  
-  if (authRoutes.includes(path) && isLoggedIn) {
-    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) return;
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
   }
 
-  if (!isLoggedIn && !publicRoutes.includes(path)) {
-    const callbackUrl = encodeURIComponent(path + nextUrl.search);
-    return Response.redirect(new URL(`/auth/login?callbackUrl=${callbackUrl}`, nextUrl));
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+    return Response.redirect(
+      new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+    );
   }
 });
 
 export const config = {
-  // matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
