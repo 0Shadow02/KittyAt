5dcabc177c1fc79fb180af3ecce8f380e2b18313
Shadow <dark35854@gmail.com>
2025-02-01 18:57:35 +0530

    feat:fixed_error

--- middleware.ts
+++ middleware.ts
@@ -1,40 +1,4 @@
-// middleware.ts
-import { NextRequest, NextResponse } from "next/server";
-import { apiAuthPrefix, publicRoutes, authRoutes, DEFAULT_LOGIN_REDIRECT } from "@/routes";
-
-export default function middleware(req: NextRequest) {
-  const { nextUrl } = req;
-  const isLoggedIn = !!req.cookies.get("auth_token"); // Check if there's a token or session
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-
-  if (isApiAuthRoute) return; // Allow API routes to proceed
-
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    }
-    return;
-  }
-
-  if (!isLoggedIn && !isPublicRoute) {
-    let callbackUrl = nextUrl.pathname;
-    if (nextUrl.search) {
-      callbackUrl += nextUrl.search;
-    }
-
-    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-
-    return NextResponse.redirect(
-      new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
-    );
-  }
-
-  // Allow the request to continue
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+import NextAuth from "next-auth"
+import authConfig from "./auth.config"
+ 
+export const { auth: middleware } = NextAuth(authConfig)
