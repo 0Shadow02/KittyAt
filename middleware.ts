d22d16b8703fedc01261dcd4930581e878e6fc15
Shadow <dark35854@gmail.com>
2025-02-01 21:35:39 +0530

    feat:fixed_error

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,37 @@
-import NextAuth from "next-auth";
+import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
-import authConfig from "@/auth.config";
-import {
-  DEFAULT_LOGIN_REDIRECT,
-  apiAuthPrefix,
-  authRoutes,
-  publicRoutes,
-} from "@/routes";
+export const runtime = "experimental-edge";
 
-const { auth } = NextAuth(authConfig);
+export default async function middleware(req: Request) {
+  const { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } = await import("@/routes");
 
-export default auth((req) => {
-  const { nextUrl } = req;
-  const isLoggedIn = !!req.auth;
+  const secret = process.env.AUTH_SECRET;
+  if (!secret) {
+    throw new Error("Missing AUTH_SECRET environment variable");
+  }
+
+  const token = await getToken({ req, secret });
+  const isLoggedIn = !!token;
+
+  const nextUrl = new URL(req.url);
+
+  // Bypass authentication for static assets and specific API route
+  if (nextUrl.pathname.startsWith('/_next/') || nextUrl.pathname.includes('.') && !nextUrl.pathname.endsWith('.html') || nextUrl.pathname.startsWith("/api/v1/events")) {
+    return;
+  }
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  if (isApiAuthRoute) return;
+  if (isApiAuthRoute) {
+    return;
+  }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
     return;
   }
@@ -33,14 +42,15 @@ export default auth((req) => {
       callbackUrl += nextUrl.search;
     }
 
-    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-
-    return Response.redirect(
-      new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+    return NextResponse.redirect(
+      new URL(`/auth/login?callbackUrl=${encodeURIComponent(callbackUrl)}`, nextUrl)
     );
   }
-});
 
+  return undefined;
+}
+
+// Optionally, don't invoke Middleware on some paths
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
